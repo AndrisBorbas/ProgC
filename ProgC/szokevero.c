e1e8a7aadb5f2118e73c6b6620b210153931f412
AndrisBorbas <andris.borbas@gmail.com>
2018-10-05 17:47:37 +0200

    squash bugs.

--- ProgC/szokevero.c
+++ ProgC/szokevero.c
@@ -1,8 +1,8 @@
-﻿#include <math.h>
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <time.h>
 
 //Megcseréli a két chart
 void csere(char*, int, int);
@@ -14,16 +14,15 @@ void kever(char*, int);
 void szokevero() {
 	char szo[50], szoveg[100000] = { '\0' };
 	int n = sizeof(szo) / sizeof(szo[0]);
-	//szavanként beolvas??
-	while (scanf("%s", &szo)) {
-		//"exit" re kiírja az eddig beírt szöveget
+	int k = 0;
+	//nem akar enterrel kijönni, "exit" vagy ctrl+z beírásával jön ki
+	while (scanf("%s", szo) != 0) {
 		if (strcmp(szo, "exit") == 0)break;
-
 		kever(szo, n);
-
 		//szöveg eltárolása
 		strcat(szoveg, szo);
 		strcat(szoveg, " ");
+		k++;
 	}
 	printf("%s", szoveg);
 }
@@ -38,9 +37,11 @@ void csere(char* szo, int i, int j) {
 void kever(char* szo, int n) {
 	srand(time(NULL));
 	int db = 2;
+	//skip ha a szó kevesebb mint 4 karakter
+	if (strlen(szo) < 4)return;
 	//írásjelek detektálása
 	if (szo[strlen(szo) - 1] == '.' || szo[strlen(szo) - 1] == '!' || szo[strlen(szo) - 1] == '?' || szo[strlen(szo) - 1] == ',')db = 3;
-	//... detektálása
+	//"..." detektálása
 	if (szo[strlen(szo) - 3] == '.')db = 5;
 	for (int i = 1; i < strlen(szo) - db; i++) {
 		// random index generálása cseréhez
--- ProgC/test.c
+++ ProgC/test.c
@@ -17,6 +17,14 @@
 /// Tests this instance.
 /// </summary>
 void test() {
-	int x=delay(1000);
-	printf("%d", x);
+	char str1[20], str2[30];
+
+	printf("Enter name: ");
+	scanf("%s", str1);
+
+	printf("Enter your website name: ");
+	scanf("%s", str2);
+
+	printf("Entered Name: %s\n", str1);
+	printf("Entered Website:%s", str2);
 }
